e2e122049de62ff44b5ebcf6a0ba3bbcb17bbd1e
Dawit Mengistu <davemetadell@gmail.com>
2024-12-17 10:51:40 +0300

    dummy fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const PORT = 5005
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
@@ -8,6 +8,6 @@ app.get("/", function (req, res) {
 });
 
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000")
+app.listen(PORT, function () {
+    console.log("Server started on port " + PORT)
 });
